f17fd9c42abbc055a5458e0ff4962db40ab9c200
fateme <fateme.soleimani.d@gmail.com>
2025-12-16 09:58:05 +0330

    add models in panel admin

--- apps/finance/admin.py
+++ apps/finance/admin.py
@@ -1,3 +1,55 @@
 from django.contrib import admin
 
-# Register your models here.
+from apps.finance.models import *
+
+
+@admin.register(ChargeRequest)
+class ChargeRequestAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "user",
+        "amount",
+        "status",
+        "approved_by",
+        "created_at",
+    )
+    list_filter = ("status", "created_at")
+    search_fields = ("user__username",)
+    readonly_fields = ("user", "amount", "created_at")
+
+    ordering = ("-created_at",)
+
+
+@admin.register(Wallet)
+class WalletAdmin(admin.ModelAdmin):
+    list_display = ("id", "user", "balance", "created_at")
+    search_fields = ("user__username",)
+    readonly_fields = ("user", "balance", "created_at")
+
+    ordering = ("-created_at",)
+
+
+@admin.register(Transaction)
+class TransactionAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "wallet",
+        "amount",
+        "type",
+        "charge_target",
+        "created_at",
+    )
+    list_filter = ("type", "created_at")
+    search_fields = (
+        "wallet__user__username",
+        "charge_target",
+    )
+    readonly_fields = (
+        "wallet",
+        "amount",
+        "type",
+        "charge_target",
+        "created_at",
+    )
+
+    ordering = ("-created_at",)
--- apps/user/admin.py
+++ apps/user/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
 
-# Register your models here.
+from apps.user.models import User
+
+
+@admin.register(User)
+class UserAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "username",
+        "is_superuser",
+        "wallet_balance",
+        "is_active",
+        "date_joined",
+    )
+
+    def wallet_balance(self, obj):
+        if hasattr(obj, "wallet"):
+            return obj.wallet.balance
+        return None
+
+    wallet_balance.short_description = "Balance"
